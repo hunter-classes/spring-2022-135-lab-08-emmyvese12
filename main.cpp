89e0a0678a8201b15586a8151a4752cec1bdc14d
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-23 19:43:20 -0400

    updated task A

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 int main(){
 
     //task A invert colors
-    std::string imageFile = "inImage.pgm";
+    std::string imageFile = "image1.pgm";
     std::string taskA_outfile = "taskA.pgm";
     invertColors(imageFile, taskA_outfile); // on mac--> open taskA.pgm OR qlmanage -p taskA.pgm
 
