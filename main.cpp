f64a81df7b67384f9a3c5a6ecc8005398112ef81
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-23 19:34:01 -0400

    added task A

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include "imageio.h"
+#include "invertcolors.h"
+
+int main(){
+
+    //task A invert colors
+    std::string imageFile = "inImage.pgm";
+    std::string taskA_outfile = "taskA.pgm";
+    invertColors(imageFile, taskA_outfile); // on mac--> open taskA.pgm OR qlmanage -p taskA.pgm
+
+
+    return 0;
+}
