5ebcdbb0c30c9eced329c00924f5dea9fecc7f72
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-24 01:05:45 -0400

    added frame function for task D

--- taskfuncs.cpp
+++ taskfuncs.cpp
@@ -60,8 +60,8 @@ void invertColors2(std::string imagefile, std::string outfile){
 }
 
 
-// task C
-// add a white box exactly in the middle of the picture 
+//task C
+//add a white box exactly in the middle of the picture 
 
 void whiteBox(std::string imagefile, std::string outfile){
 
@@ -84,6 +84,50 @@ void whiteBox(std::string imagefile, std::string outfile){
             if ( (col >= colMark1 && col < colMark2) && (row >= rowMark1 && row < rowMark2) ){
                 int diff = 255 - image[row][col]; // to be white, the pixel brightness needs to be 255, so we calculate the difference then add that back
                 out[row][col] = diff + (image[row][col]);
+            }
+            else {
+                out[row][col] = image[row][col];
+            }
+        }
+    }
+
+    writeImage(outfile, out, height, width);
+
+}
+
+
+//task D
+//add a frame (one pixel thick) in the middle of the picture
+
+void addFrame(std::string imagefile, std::string outfile){
+    
+    int image[MAX_H][MAX_W];
+    int height, width;
+
+    readImage(imagefile, image, height, width);
+
+    int out[MAX_H][MAX_W];
+    int colMark1 = width/4; // quarter of the width (left side of the box)
+    int colMark2 = 3 * colMark1; // 3/4 of the width (right side of the box)
+
+    int rowMark1 = height/4; // quarter of the height (top of the box)
+    int rowMark2 = 3 * rowMark1; // 3/4 of the height (bottom of the box)
+
+    for (int row = 0; row < height; row++){
+        for (int col = 0; col < width; col++){
+
+            // top horizontal line must be on rowMark1 and can span through the columns
+            // left vertical line must be on colMark1 and can span through all the rows
+            // (same applies for right line and bottom line)
+
+            // the column must be in between the column range and must belong on either marked row
+            if ( (col >= colMark1 && col < colMark2) && (row == rowMark1 || row == rowMark2) ){
+                int diff = 255 - image[row][col];
+                out[row][col] = diff + image[row][col];
+            }
+            else if ( (row >= rowMark1 && row < rowMark2) && (col == colMark1 || col == colMark2) ){
+                int diff = 255 - image[row][col];
+                out[row][col] = diff + image[row][col];
             }
             else {
                 out[row][col] = image[row][col];
