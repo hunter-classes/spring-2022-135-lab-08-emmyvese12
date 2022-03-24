5e6d8dc5038a5f2b33f0f39b2aa3dc24a04520f5
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-24 01:06:09 -0400

    added function declaration for task D

--- taskfuncs.h
+++ taskfuncs.h
@@ -6,3 +6,5 @@ void invertColors(std::string imagefile, std::string outfile);
 void invertColors2(std::string imagefile, std::string outfile);
 
 void whiteBox(std::string imagefile, std::string outfile);
+
+void addFrame(std::string imagefile, std::string outfile);
