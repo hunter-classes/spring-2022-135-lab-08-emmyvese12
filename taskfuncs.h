a4fb9f9a9c233e64adf0cb0a337ceda9fda84263
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-24 20:30:37 -0400

    added function declarations for task E and F

--- taskfuncs.h
+++ taskfuncs.h
@@ -8,3 +8,7 @@ void invertColors2(std::string imagefile, std::string outfile);
 void whiteBox(std::string imagefile, std::string outfile);
 
 void addFrame(std::string imagefile, std::string outfile);
+
+void scaleBy200(std::string imagefile, std::string outfile);
+
+void pixelate(std::string imagefile, std::string outfile);
