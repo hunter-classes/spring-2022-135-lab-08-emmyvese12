8920da13350baac015e3c8836db3f0ee0afa4293
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-24 20:29:31 -0400

    added task E and F

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+Emmy Veselinov
+CSCI 135 Lab-08
+Image Processing
+*/
+//task A-F 
+
 #include <iostream>
 #include <string>
 #include <cassert>
@@ -9,7 +16,7 @@
 int main(){
 
     //task A invert colors
-    std::string imageFile = "image1.pgm";
+    std::string imageFile = "catimage.pgm";
     std::string taskA_outfile = "taskA.pgm";
     invertColors(imageFile, taskA_outfile); // on mac--> open taskA.pgm OR qlmanage -p taskA.pgm
 
@@ -24,6 +31,14 @@ int main(){
     //task D add a white frame
     std::string taskD_outfile = "taskD.pgm";
     addFrame(imageFile, taskD_outfile);
+
+    //task E scale by 200%
+    std::string taskE_outfile = "taskE.pgm";
+    scaleBy200(imageFile, taskE_outfile);
+
+    //task F pixelate the image file
+    std::string taskF_outfile = "taskF.pgm";
+    pixelate(imageFile, taskF_outfile);
 
     return 0;
 }
